fc1e3aeda821d29c0e681e5a8a6981ae1010649a
Erik Huang <erikhuangdrums@gmail.com>
2021-03-15 10:30:41 -0700

    commit tree e2f20eec5efc2d1da628c4bce18feea91702542d

--- src/Bomb.js
+++ src/Bomb.js
@@ -1,1 +1,21 @@
-// your Bomb code here!
+import React from 'react';
+
+class Bomb extends React.Component {
+    state = { secondsLeft: this.props.initialCount }
+
+    setBombMsg = () => {
+        const {secondsLeft} = this.state;
+        if (secondsLeft === 0) {
+            return 'Boom!';
+        }
+        return `${secondsLeft} seconds left before I go boom!`;
+    }
+
+    render() {
+        return (
+            <div>{this.setBombMsg()}</div>
+        );
+    }
+}
+
+export default Bomb;
